3b69e3561c289cb723a1efbfc80b88e532fd3f01
Prosper <prosperorjiogo1999@gmail.com>
2023-10-31 23:34:27 +0100

    control flow

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{}{}" .format(i,j), end=', ')
+        if i == 9 and j == 9:
+            print("{}{}" .format(i,j), end=', ')
