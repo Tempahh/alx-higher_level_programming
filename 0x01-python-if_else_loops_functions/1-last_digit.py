e2f9bcd5d9a0f4650584677df24442a97f71e73e
Prosper <prosperorjiogo1999@gmail.com>
2023-10-31 22:45:24 +0100

    control flow

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number % 10 > 5:
+if abs(number) % 10 > 5:
     print("Last digit of {} is {} and is greater than 5\n" .format(number, number % 10))
-elif number % 10 == 0:
+elif abs(number) % 10 == 0:
     print("Last digit of {} is {} and is 0\n" .format(number, number % 10))
-elif number % 10 < 6 & number % 10 != 0:
+elif abs(number) % 10 < 6 & number % 10 != 0:
     print("Last digit of {} is {} and is less than 6 and not 0\n" .format(number, number % 10))
