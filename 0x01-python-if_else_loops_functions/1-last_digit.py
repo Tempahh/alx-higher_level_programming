73d594b34999020348f3de71b3068bb0a8aaf319
Prosper <prosperorjiogo1999@gmail.com>
2023-10-31 22:49:17 +0100

    control flow

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,5 +5,5 @@ if abs(number) % 10 > 5:
     print("Last digit of {} is {} and is greater than 5\n" .format(number, number % 10))
 elif abs(number) % 10 == 0:
     print("Last digit of {} is {} and is 0\n" .format(number, number % 10))
-elif abs(number) % 10 < 6 & number % 10 != 0:
+elif abs(number) % 10 < 6 and number % 10 != 0:
     print("Last digit of {} is {} and is less than 6 and not 0\n" .format(number, number % 10))
