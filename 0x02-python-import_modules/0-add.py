e15fa276dc156de08333b9b684b0429edb3928a9
Prosper <prosperorjiogo1999@gmail.com>
2023-11-02 22:58:04 +0100

    first import task

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,8 @@
-#!/bin/python
+#!/usr/bin/python
 if __name__ == "__main__":
     """print sum"""
     from add_0 import add
     
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}" .format(a, b, add(a, b)))
+    print("{} + {} = {}" .format(a, b, add(a, b)))
