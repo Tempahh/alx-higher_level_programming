d9fc1bd2cc74fcf797be2babe49c396ad2dd73dd
Prosper <prosperorjiogo1999@gmail.com>
2023-11-07 02:22:30 +0100

    data structures

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    if len(sentence) < 0:
-        return None
-    else:
-        tuple_result = sentence[0]
-        return (len(sentence), tuple_result)
+    if sentence == "":
+        return (0, None)
+
+    return (len(sentence), sentence[0])
         
