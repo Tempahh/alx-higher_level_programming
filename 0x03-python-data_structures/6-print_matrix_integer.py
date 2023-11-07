9afcbeb07eebf5681dadf1e59274158ab43b7d19
Prosper <prosperorjiogo1999@gmail.com>
2023-11-07 02:25:04 +0100

    data structures

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for columns in row:
-            print("{:4}" .format(columns), end='')
-        print()
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+                print("{:d}".format(matrix[i][j]), end="")
+                if j != (len(matrix[i]) - 1):
+                    print(" ", end="")
+
+        print("")
