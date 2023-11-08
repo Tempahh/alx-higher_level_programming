63ebe4c59bc11ab97151439bfc5f90fd4f94a860
Prosper <prosperorjiogo1999@gmail.com>
2023-11-08 23:36:16 +0100

    tasks on more data structures

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,6 @@
 #!/Users/mac/opt/anaconda3/bin/python
 #Tempah
+
 def square_matrix_simple(matrix=[]):
-    sq_val = [[x**2 for x in row] for row in matrix]
-    return sq_val
+    """Compute the square value of all integers of a matrix."""
+    return ([list(map(lambda x: x * x, row)) for row in matrix])
